229d511ff7d5d8ccbb2fdc20ea5c885d295bd053
Ryan Ouyang <ryan.ouyang01@gmail.com>
2024-04-16 02:59:45 -0400

    Inherit from reference simple ERC6551Account

--- src/ERC6551CTGVault.sol
+++ src/ERC6551CTGVault.sol
@@ -9,42 +9,10 @@ import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
 
 import "erc6551/src/interfaces/IERC6551Registry.sol";
+import "erc6551/src/examples/simple/ERC6551Account.sol";
 
-interface IERC6551Account {
-    receive() external payable;
-
-    function token()
-        external
-        view
-        returns (uint256 chainId, address tokenContract, uint256 tokenId);
-
-    function state() external view returns (uint256);
-
-    function isValidSigner(
-        address signer,
-        bytes calldata context
-    ) external view returns (bytes4 magicValue);
-}
-
-interface IERC6551Executable {
-    function execute(
-        address to,
-        uint256 value,
-        bytes calldata data,
-        uint8 operation
-    ) external payable returns (bytes memory);
-}
-
-contract ERC6551CTGVault is
-    IERC165,
-    IERC1271,
-    IERC6551Account,
-    IERC6551Executable,
-    ERC721Holder
-{
+contract ERC6551CTGVault is ERC721Holder, ERC6551Account {
     using EnumerableMap for EnumerableMap.UintToAddressMap;
-
-    uint256 public state;
 
     // ERC6551 constants
     address public constant registry =
@@ -65,14 +33,12 @@ contract ERC6551CTGVault is
     uint256 public amountPerStaker;
     bool public isWithdrawalEnabled;
 
-    receive() external payable {}
-
     function execute(
         address to,
         uint256 value,
         bytes calldata data,
         uint8 operation
-    ) external payable virtual returns (bytes memory result) {
+    ) external payable virtual override returns (bytes memory result) {
         require(_isValidSigner(msg.sender), "Invalid signer");
         require(operation == 0, "Only call operations are supported");
 
@@ -92,66 +58,6 @@ contract ERC6551CTGVault is
                 revert(add(result, 32), mload(result))
             }
         }
-    }
-
-    function isValidSigner(
-        address signer,
-        bytes calldata
-    ) external view virtual returns (bytes4) {
-        if (_isValidSigner(signer)) {
-            return IERC6551Account.isValidSigner.selector;
-        }
-
-        return bytes4(0);
-    }
-
-    function isValidSignature(
-        bytes32 hash,
-        bytes memory signature
-    ) external view virtual returns (bytes4 magicValue) {
-        bool isValid = SignatureChecker.isValidSignatureNow(
-            owner(),
-            hash,
-            signature
-        );
-
-        if (isValid) {
-            return IERC1271.isValidSignature.selector;
-        }
-
-        return bytes4(0);
-    }
-
-    function supportsInterface(
-        bytes4 interfaceId
-    ) public view virtual returns (bool) {
-        return
-            interfaceId == type(IERC165).interfaceId ||
-            interfaceId == type(IERC6551Account).interfaceId ||
-            interfaceId == type(IERC6551Executable).interfaceId;
-    }
-
-    function token() public view virtual returns (uint256, address, uint256) {
-        bytes memory footer = new bytes(0x60);
-
-        assembly {
-            extcodecopy(address(), add(footer, 0x20), 0x4d, 0x60)
-        }
-
-        return abi.decode(footer, (uint256, address, uint256));
-    }
-
-    function owner() public view virtual returns (address) {
-        (uint256 chainId, address tokenContract, uint256 tokenId) = token();
-        if (chainId != block.chainid) return address(0);
-
-        return IERC721(tokenContract).ownerOf(tokenId);
-    }
-
-    function _isValidSigner(
-        address signer
-    ) internal view virtual returns (bool) {
-        return signer == owner();
     }
 
     function onERC721Received(
